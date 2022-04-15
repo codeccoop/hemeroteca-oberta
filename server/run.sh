93148b772cb6d01669cdebf84473da9aefcb896a
Orzo Cogorzo <orzocogorzo@hotmail.com>
2022-04-15 20:04:05 +0200

    Set run script more verbose after switch to nohup execution

--- server/run.sh
+++ server/run.sh
@@ -5,9 +5,11 @@ then
     kill $(cat process.pid)
 fi
 
+echo "Start running server"
 OH_ENV=production nohup .venv/bin/uvicorn --proxy-headers main:app &>/dev/null &
 
 pid=$!
 echo "$pid" > process.pid
+echo "Running server with PID $pid"
 
 # .venv/bin/uvicorn --proxy-headers --forwarded-allow-ips='*' --uds /run/uvicorn/hemeroteca-oberta.sock main:app
